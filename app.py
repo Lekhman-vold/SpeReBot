745c1d69643bee17ed40c3d028a922ea04575501
Lekhman-vold <volodya.l@yahoo.com>
2021-05-29 23:25:10 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+import telebot
+
+
+bot = telebot.TeleBot('1835070518:AAHgWVPfXMqkhQX2UkpaBmolqeMi4qdwk5Q')
+bot.set_webhook(url="https://ed20d6fb8471.ngrok.io")
+app = Flask(__name__)
+
+
+@app.route('/', methods=["POST"])
+def webhook():
+    bot.process_new_updates(
+        [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))]
+    )
+    return "ok"
+
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    bot.send_message(message.chat.id, 'Hello!')
+
+
+@bot.message_handler(commands=['test'])
+def s(message):
+    bot.send_message(message.chat.id, 'Hello!')
+
+if __name__ == "__main__":
+    app.run()
