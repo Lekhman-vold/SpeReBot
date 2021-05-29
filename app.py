02db4b6358af7b795bfb7805b3424c65b719f145
Lekhman-vold <volodya.l@yahoo.com>
2021-05-29 23:29:31 +0300

    del func

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import telebot
 
 
-bot = telebot.TeleBot('')
+bot = telebot.TeleBot('1835070518:AAHgWVPfXMqkhQX2UkpaBmolqeMi4qdwk5Q')
 bot.set_webhook(url="https://ed20d6fb8471.ngrok.io")
 app = Flask(__name__)
 
@@ -19,10 +19,6 @@ def webhook():
 def start_command(message):
     bot.send_message(message.chat.id, 'Hello!')
 
-
-@bot.message_handler(commands=['test'])
-def s(message):
-    bot.send_message(message.chat.id, 'Hello!')
 
 if __name__ == "__main__":
     app.run()
